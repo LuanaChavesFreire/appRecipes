633804656d56caf23f78bd54f9d780723af5d328
LuanaChavesFreire <luanachavesfreire2006@gmail.com>
2025-11-02 16:50:21 -0300

    feat: recipe list logic pt1

--- app/screens/inApp/recipeView/index.jsx
+++ app/screens/inApp/recipeView/index.jsx
@@ -1,21 +1,29 @@
+import { useEffect, useState } from 'react';
 import { Text, View, ScrollView, TouchableWithoutFeedback } from "react-native"
 import Styles from './style'
 import api from "../../../../services/api"
 import AsyncStorage from "@react-native-async-storage/async-storage"
 
 function ViewRecipe() {
-    async function getRecipes() {
-        try{
-            const token = await AsyncStorage.getItem("token")
-            const res = await api.get('/recipes', {headers: {'Authorization': `Bearer ${token}`}});
-            console.log(res.data)
+    const [recipes, setRecipes] = useState([]);
+
+    useEffect(() => {
+        async function getRecipes() {
+            try {
+                const token = await AsyncStorage.getItem("token")
+                const res = await api.get('/recipes', { headers: { 'Authorization': `Bearer ${token}` } });
+                console.log(res.data)
+                setRecipes(res.data)
+                console.log(recipes)
+            }
+            catch (err) {
+                console.log(err);
+
+            }
         }
-        catch(err){
-            console.log(err);
-            
-        }
-    }
-    getRecipes();
+        getRecipes();
+    }, [])
+
     return (
         <View style={Styles.container}>
             <View style={Styles.header}>
@@ -23,22 +31,23 @@ function ViewRecipe() {
                     What Are We Cooking Today?
                 </Text>
             </View>
+
             <View style={Styles.listContainer}>
                 <Text style={Styles.listTittle}>
                     Ur Recipes
                 </Text>
+
                 <View style={Styles.list}>
                     <ScrollView>
-                        <TouchableWithoutFeedback>
-                            <View style={Styles.btnRecipe}>
-                                <Text style={Styles.txtBtnRecipe}>Titulo da receita</Text>
-                            </View>
-                        </TouchableWithoutFeedback>
-                        <TouchableWithoutFeedback>
-                            <View style={Styles.btnRecipe}>
-                                <Text style={Styles.txtBtnRecipe}>Titulo da receita</Text>
-                            </View>
-                        </TouchableWithoutFeedback>
+                        {recipes.map((item) => {
+                            return (
+                                <TouchableWithoutFeedback key={item.tittle}>
+                                    <View style={Styles.btnRecipe}>
+                                        <Text style={Styles.txtBtnRecipe}>{item.tittle}</Text>
+                                    </View>
+                                </TouchableWithoutFeedback>
+                            )
+                        })}
                     </ScrollView>
                 </View>
             </View>
